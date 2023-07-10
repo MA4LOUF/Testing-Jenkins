a73851fd5105af1ee0fb761979f02a83e28fd2e8
Christopher Maalouf <christophermaalouf123@gmail.com>
2023-07-10 10:11:00 +0300

    ADDING A SAMPLE TEST

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,11 @@
+import Classes.Person;
 public class MainTest {
-
+    public static void printJob(Person person){
+        System.out.println(person.getJob());
+    }
+    public static void main(String[] args) {
+        Person chris = new Person("Chris", 16, "Student");
+        System.out.println(chris);
+        printJob(chris);
+    }
 }
